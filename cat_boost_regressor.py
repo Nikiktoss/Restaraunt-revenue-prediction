5bb1c7807b91554ccc197f0ab1b45452d153e152
Nikita Chebotarevskiy <mikita.chebotarevskiy@gmail.com>
2023-03-26 09:18:08 +0300

    make RevenuePredictionModel class

--- cat_boost_regressor.py
+++ cat_boost_regressor.py
@@ -9,6 +9,75 @@ from sklearn.metrics import mean_squared_error as mse, r2_score, mean_absolute_e
 import matplotlib.pyplot as plt
 
 
+class RevenuePredictionModel:
+    def __init__(self):
+        self.model = CatBoostRegressor(n_estimators=250, loss_function="RMSE", learning_rate=0.524, depth=3,
+                                       task_type='CPU', verbose=False)
+        self.cat_encoder = ce.cat_boost.CatBoostEncoder()
+        self.min_values = None
+        self.max_values = None
+        self.w = None
+
+    @staticmethod
+    def divide_data(data):
+        now = datetime.datetime.now()
+
+        cat_data = data[['City', 'City Group', 'Type']]
+
+        num_data = data.drop(['Id', 'City', 'City Group', 'Type', 'Open Date'], axis=1)
+        num_data['years_old'] = (now - pd.DatetimeIndex(data['Open Date'])).days // 365
+
+        return num_data, cat_data
+
+    def normalize_train_data(self, num_data):
+        min_values = num_data.min()
+        max_values = num_data.max()
+
+        num_data = (num_data - min_values) / (max_values - min_values)
+        self.min_values = min_values
+        self.max_values = max_values
+
+        return num_data
+
+    def prepare_train_data(self, data):
+        train_num_data, train_cat_data = self.divide_data(data.drop('revenue', axis=1))
+        train_num_data = self.normalize_train_data(train_num_data)
+
+        w = sum(data["revenue"])
+        self.w = w
+
+        y_train = np.sqrt(data["revenue"]) / np.sqrt(w)
+        x_train = pd.concat([train_cat_data, train_num_data], axis=1)
+
+        self.cat_encoder.fit(x_train, y_train)
+        x_train = self.cat_encoder.transform(x_train)
+
+        return x_train, y_train
+
+    def fit_model(self, data):
+        x_train, y_train = self.prepare_train_data(data)
+        self.model.fit(x_train, y_train)
+
+    def normalize_data(self, data):
+        data = (data - self.min_values) / (self.max_values - self.min_values)
+        return data
+
+    def prepare_data(self, data):
+        num_data, cat_data = self.divide_data(data)
+        num_data = self.normalize_data(num_data)
+
+        x = pd.concat([cat_data, num_data], axis=1)
+        x = self.cat_encoder.transform(x)
+
+        return x
+
+    def predict_revenue(self, data):
+        x = self.prepare_data(data)
+        revenue = np.power(self.model.predict(x) * np.sqrt(self.w), 2)
+
+        return revenue
+
+
 train_data = pd.read_csv('train.csv')
 test_data = pd.read_csv('test.csv')
 test_valid_data = train_data.loc[123:, :]
@@ -19,55 +88,11 @@ train_cols = train_data.columns
 test_valid_data = pd.DataFrame(np.array(test_valid_data), columns=train_cols)
 
 
-def divide_data(data):
-    now = datetime.datetime.now()
+revenue_model = RevenuePredictionModel()
+revenue_model.fit_model(train_data)
 
-    cat_data = data[['City', 'City Group', 'Type']]
-
-    num_data = data.drop(['Id', 'City', 'City Group', 'Type', 'Open Date', 'revenue'], axis=1)
-    num_data['years_old'] = (now - pd.DatetimeIndex(data['Open Date'])).days // 365
-
-    return num_data, cat_data
-
-
-def normalize_train_data(data):
-    min_values = data.min()
-    max_values = data.max()
-
-    data = (data - min_values) / (max_values - min_values)
-    return data, min_values, max_values
-
-
-def normalize_test_data(data, min_values, max_values):
-    data = (data - min_values) / (max_values - min_values)
-    return data
-
-
-train_num_data, train_cat_data = divide_data(train_data)
-train_num_data, min_norm, max_norm = normalize_train_data(train_num_data)
-
-w = sum(train_data["revenue"])
-y_train = np.sqrt(train_data["revenue"]) / np.sqrt(w)
-x_train = pd.concat([train_cat_data, train_num_data], axis=1)
-
-cbe_encoder = ce.cat_boost.CatBoostEncoder()
-cbe_encoder.fit(x_train, y_train)
-x_train = cbe_encoder.transform(x_train)
-
-
-cb = CatBoostRegressor(n_estimators=250, loss_function="RMSE", learning_rate=0.524, depth=3, task_type='CPU',
-                       verbose=False)
-cb.fit(x_train, y_train)
-
-
-test_num_data, test_cat_data = divide_data(test_valid_data)
-test_num_data = normalize_test_data(test_num_data, min_norm, max_norm)
-
-x_test = pd.concat([test_cat_data, test_num_data], axis=1)
-x_test = cbe_encoder.transform(x_test)
 y_test = test_valid_data['revenue']
-
-y_predict = np.power(cb.predict(x_test) * np.sqrt(w), 2)
+y_predict = revenue_model.predict_revenue(test_valid_data.drop('revenue', axis=1))
 
 print(f"R2 is {r2_score(y_test, y_predict)}")
 cb_rmse = np.sqrt(mse(y_test, y_predict))
