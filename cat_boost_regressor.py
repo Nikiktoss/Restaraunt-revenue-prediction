f398a83a8121944e1df791b91fdd9dfece671b17
Nikita Chebotarevskiy <mikita.chebotarevskiy@gmail.com>
2023-03-28 12:36:05 +0300

    dump model to file

--- cat_boost_regressor.py
+++ cat_boost_regressor.py
@@ -4,15 +4,12 @@ import category_encoders as ce
 import pandas as pd
 import numpy as np
 import datetime
-from sklearn.metrics import mean_squared_error as mse, r2_score, mean_absolute_error as mae
 
-import matplotlib.pyplot as plt
+import pickle
 
 
-class RevenuePredictionModel:
+class NormalizeDataClass:
     def __init__(self):
-        self.model = CatBoostRegressor(n_estimators=250, loss_function="RMSE", learning_rate=0.524, depth=3,
-                                       task_type='CPU', verbose=False)
         self.cat_encoder = ce.cat_boost.CatBoostEncoder()
         self.min_values = None
         self.max_values = None
@@ -39,7 +36,7 @@ class RevenuePredictionModel:
 
         return num_data
 
-    def prepare_train_data(self, data):
+    def prepare_train_data(self, data=pd.read_csv('train.csv')):
         train_num_data, train_cat_data = self.divide_data(data.drop('revenue', axis=1))
         train_num_data = self.normalize_train_data(train_num_data)
 
@@ -56,7 +53,7 @@ class RevenuePredictionModel:
 
     def fit_model(self, data):
         x_train, y_train = self.prepare_train_data(data)
-        self.model.fit(x_train, y_train)
+        return x_train, y_train
 
     def normalize_data(self, data):
         data = (data - self.min_values) / (self.max_values - self.min_values)
@@ -73,9 +70,7 @@ class RevenuePredictionModel:
 
     def predict_revenue(self, data):
         x = self.prepare_data(data)
-        revenue = np.power(self.model.predict(x) * np.sqrt(self.w), 2)
-
-        return revenue
+        return x
 
 
 train_data = pd.read_csv('train.csv')
@@ -87,18 +82,26 @@ train_data = train_data.iloc[:123, :]
 train_cols = train_data.columns
 test_valid_data = pd.DataFrame(np.array(test_valid_data), columns=train_cols)
 
+if __name__ == '__main__':
+    normalizer = NormalizeDataClass()
+    cb = CatBoostRegressor(n_estimators=195, loss_function="RMSE", learning_rate=0.5, depth=3, task_type='CPU',
+                           verbose=False)
 
-revenue_model = RevenuePredictionModel()
-revenue_model.fit_model(train_data)
+    x_tr, y_tr = normalizer.fit_model(train_data)
+    cb.fit(x_tr, y_tr)
 
-y_test = test_valid_data['revenue']
-y_predict = revenue_model.predict_revenue(test_valid_data.drop('revenue', axis=1))
+    with open('model.pkl', 'wb') as f:
+        pickle.dump(cb, f)
 
-print(f"R2 is {r2_score(y_test, y_predict)}")
-cb_rmse = np.sqrt(mse(y_test, y_predict))
-print(f"MAE is {mae(y_test, y_predict)}")
-print("RMSE in y units:", np.mean(cb_rmse))
+    # y_test = test_valid_data['revenue']
+    # x_test = normalizer.prepare_data(test_valid_data.drop('revenue', axis=1))
+    # y_predict = np.power(cb.predict(x_test) * np.sqrt(normalizer.w), 2)
+    #
+    # print(f"R2 is {r2_score(y_test, y_predict)}")
+    # cb_rmse = np.sqrt(mse(y_test, y_predict))
+    # print(f"MAE is {mae(y_test, y_predict)}")
+    # print("RMSE in y units:", np.mean(cb_rmse))
 
-plt.plot(y_test, 'ro')
-plt.plot(y_predict, 'go')
-plt.show()
+    # plt.plot(y_test, 'ro')
+    # plt.plot(y_predict, 'go')
+    # plt.show()
