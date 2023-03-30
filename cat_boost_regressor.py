5f720ef856b09115ec7e823d84d0de25752ab6cb
Nikita Chebotarevskiy <mikita.chebotarevskiy@gmail.com>
2023-03-31 02:49:21 +0300

    final version

--- cat_boost_regressor.py
+++ cat_boost_regressor.py
@@ -4,8 +4,14 @@ import category_encoders as ce
 import pandas as pd
 import numpy as np
 import datetime
+import random
 
 import pickle
+
+
+seed = 0
+random.seed(seed)
+np.random.seed(seed)
 
 
 class NormalizeDataClass:
@@ -74,34 +80,17 @@ class NormalizeDataClass:
 
 
 train_data = pd.read_csv('train.csv')
-test_data = pd.read_csv('test.csv')
-test_valid_data = train_data.loc[123:, :]
-
 train_data = train_data.iloc[:123, :]
 
-train_cols = train_data.columns
-test_valid_data = pd.DataFrame(np.array(test_valid_data), columns=train_cols)
+normalizer = NormalizeDataClass()
+x_tr, y_tr = normalizer.fit_model(train_data)
+
 
 if __name__ == '__main__':
-    normalizer = NormalizeDataClass()
     cb = CatBoostRegressor(n_estimators=195, loss_function="RMSE", learning_rate=0.5, depth=3, task_type='CPU',
                            verbose=False)
 
-    x_tr, y_tr = normalizer.fit_model(train_data)
     cb.fit(x_tr, y_tr)
 
     with open('model.pkl', 'wb') as f:
         pickle.dump(cb, f)
-
-    # y_test = test_valid_data['revenue']
-    # x_test = normalizer.prepare_data(test_valid_data.drop('revenue', axis=1))
-    # y_predict = np.power(cb.predict(x_test) * np.sqrt(normalizer.w), 2)
-    #
-    # print(f"R2 is {r2_score(y_test, y_predict)}")
-    # cb_rmse = np.sqrt(mse(y_test, y_predict))
-    # print(f"MAE is {mae(y_test, y_predict)}")
-    # print("RMSE in y units:", np.mean(cb_rmse))
-
-    # plt.plot(y_test, 'ro')
-    # plt.plot(y_predict, 'go')
-    # plt.show()
